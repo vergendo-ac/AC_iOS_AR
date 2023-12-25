e138f8986489a7352e05b76838692ff99ee40a7d
Andrei Okoneshnikov <andryoko@gmail.com>
2023-12-25 20:34:29 +0700

    fixed addresses

--- Sources/AC_iOS_AR/Services/Net/Servers.swift
+++ Sources/AC_iOS_AR/Services/Net/Servers.swift
@@ -12,13 +12,11 @@ class Servers {
     
     public static let addresses: [String] = [
         "https://developer.augmented.city",
-        "http://developer.augmented.city:15000",
-        "http://developer.vergendo.com",
-        "http://developer.vergendo.com:15000",
-        "http://developer.vergendo.com:25000",
-        "http://developer.vergendo.com:35000",
-        "http://185.162.94.228:5000",
-        "http://178.249.69.197:35000"
+        "https://mirror.augmented.city",
+        "https://developer.testing.augmented.city",
+        "http://185.22.84.168:45000",
+        "http://185.162.94.228:35000",
+        "http://185.162.94.228:25000"
     ]
     
 }
